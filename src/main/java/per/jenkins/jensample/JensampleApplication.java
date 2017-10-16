655f06cecf9355c0d8772074dc5e16975cbd3cc3
Athavan <ma_athavan@hotmail.com>
2017-10-15 23:17:51 -0700

    add spring boot skeleton

---
+++ src/main/java/per/jenkins/jensample/JensampleApplication.java
@@ -0,0 +1,12 @@
+package per.jenkins.jensample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JensampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JensampleApplication.class, args);
+	}
+}
---
+++ src/test/java/per/jenkins/jensample/JensampleApplicationTests.java
@@ -0,0 +1,16 @@
+package per.jenkins.jensample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JensampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
